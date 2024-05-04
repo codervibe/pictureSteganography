386be46af6aff3a44c541efbee3598b582436f83
codervibe <3507557934@qq.com>
2024-05-04 19:31:02 +0800

    添加更多的注释

--- main/main.py
+++ main/main.py
@@ -7,7 +7,7 @@
 
 VERSION = "2.0.0"
 
-# image_utils.py
+# 导入所需的库
 import os
 from PIL import Image
 import argparse
@@ -47,13 +47,18 @@ def hide_message(image_path, message):
     :param message: 要隐藏的消息
     """
     try:
+        # 打开图像文件
         img = Image.open(image_path)
+        # 获取图像的像素数据
         data = img.getdata()
         encoded_data = []
+        # 在消息末尾添加一个空字符，以便在解码时知道消息的结束
         message += "\0"
+        # 将消息转换为二进制字符串
         message_bits = ''.join(format(ord(char), '08b') for char in message)
         message_length = len(message_bits)
 
+        # 检查消息长度是否超过了图像像素数的三倍，如果是，则无法隐藏消息
         if len(data) * 3 < message_length:
             raise ValueError("消息过长，无法隐藏在图像中。")
 
@@ -63,15 +68,18 @@ def hide_message(image_path, message):
                 raise ValueError("消息包含无效的二进制字符，无法隐藏在图像中。")
 
         index = 0
+        # 遍历图像的每个像素
         for pixel in data:
             if index < message_length:
                 encoded_pixel = list(pixel)
+                # 将消息的二进制位嵌入到像素的最低有效位中
                 encoded_pixel[-1] = int(message_bits[index])
                 encoded_data.append(tuple(encoded_pixel))
                 index += 1
             else:
                 encoded_data.append(pixel)
 
+        # 将修改后的像素数据写回图像
         img.putdata(encoded_data)
         img.save(image_path)
         print("消息隐藏成功!")
@@ -90,17 +98,22 @@ def extract_message(image_path):
     :param image_path: 图像文件的路径
     """
     try:
+        # 打开图像文件
         img = Image.open(image_path)
+        # 获取图像的像素数据
         data = img.getdata()
         extracted_bits = []
+        # 遍历图像的每个像素，并提取最后一个位，这个位就是消息的隐藏位
         for pixel in data:
             extracted_bits.append(str(pixel[-1]))
 
         binary_message = ''.join(extracted_bits)
         message = ""
+        # 将提取的二进制消息转换为字符串
         for i in range(0, len(binary_message), 8):
             byte = binary_message[i:i + 8]
             message += chr(int(byte, 2))
+            # 如果遇到空字符，则表示消息结束
             if message[-1] == "\0":
                 break
         print("提取的消息:", message.rstrip("\0"))
